f3230517e8368ea5f421d5104ba9e558d68ebd43
devFaramarzi <dev.faramarzi396@gmail.com>
2023-10-09 12:53:39 +0330

    soft delete

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">WebProg.io</Link>
+                <Link className="navbar-brand" to="/">Shopping - Cart</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
         <div className="container mt-5">
             <div className="row text-center">
                 <div className="col-md-12">
-                    <h2>Shopping Cart - WebProg.io</h2>
+                    <h2>Shopping Cart - Application</h2>
                     <p className="mt-4">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
                         totam, ratione, voluptas eveniet soluta incidunt itaque optio beatae
